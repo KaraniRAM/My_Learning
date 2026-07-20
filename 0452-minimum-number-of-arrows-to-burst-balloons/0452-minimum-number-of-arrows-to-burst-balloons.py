d59e205377c6897006815306a262b7f2534f4d55
Karani R <cdmkarani@gmail.com>
2026-07-20 11:58:37 +0530

    Refactor findMinArrowShots for clarity and efficiency

--- 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.py
+++ 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.py
@@ -12,3 +12,18 @@ class Solution(object):
                 c+=1
                 e=points[i][1]
         return c
+
+class Solution(object):
+    def findMinArrowShots(self, points):
+        """
+        :type points: List[List[int]]
+        :rtype: int
+        """
+        points.sort(key=lambda x:x[1])
+        c=0
+        e=float('-inf')
+        for i in range(len(points)):
+            if points[i][0]>e:
+                c+=1
+                e=points[i][1]
+        return c
