cc5bc900911390d38e673e2137f732fe45b78ba2
Karani R <cdmkarani@gmail.com>
2026-07-23 11:43:55 +0530

    Time: 0 ms (100%), Space: 12.4 MB (19.47%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.py
+++ 0054-spiral-matrix/0054-spiral-matrix.py
@@ -4,44 +4,32 @@ class Solution(object):
         :type matrix: List[List[int]]
         :rtype: List[int]
         """
-       
-
         i = j = 0
         r = len(matrix) - 1
         c = len(matrix[0]) - 1
         g = []
         n = 0
-
         while n <= r and n <= c:
-
             while j <= c:
                 g.append(matrix[i][j])
                 j += 1
             j -= 1
             i += 1
-
             while i <= r:
                 g.append(matrix[i][j])
                 i += 1
             i -= 1
             j -= 1
-
-            if n < r:
-                while j >= n:
+            while j >= n and n<r:
                     g.append(matrix[i][j])
                     j -= 1
-                j += 1
-
+            j += 1
             i -= 1
-
-            if n < c:
-                while i > n:
+            while i > n and n<c:
                     g.append(matrix[i][j])
                     i -= 1
-                i += 1
-
+            i += 1
             j += 1
-
             n += 1
             r -= 1
             c -= 1
